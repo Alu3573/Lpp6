d81849f9972e19c4e7feccb580e25c78d147e5a5
Pedro Javier <pedro.javier.nunez.rodriguez@gmail.com>
2012-10-26 18:33:12 +0100

    test tirada par la maquina

--- lib/ppt.rb
+++ lib/ppt.rb
@@ -1,10 +1,14 @@
 class PiedraPapelTijeras
     def initialize(opcion)
         @opcionhumano = opcion
+	@opcionmaquina = nil;
     end
     def jugadahumano()
         @opcionhumano = nil
 	print "Elije una opcion:"
 	@opcionhumano = STDIN.gets.strip.downcase
+    end
+    def jugadamaquina()
+        @opcionmaquina = nil;
     end
 end

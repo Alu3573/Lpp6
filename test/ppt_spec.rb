28cf35a585ca932d326d27f3fc2cf17721aac0e7
Pedro Javier <pedro.javier.nunez.rodriguez@gmail.com>
2012-10-26 18:28:26 +0100

    Test tirada humano

--- test/ppt_spec.rb
+++ test/ppt_spec.rb
@@ -7,3 +7,10 @@ describe PiedraPapelTijeras do
   before :each do
       @ppt = PiedraPapelTijeras.new("piedra")
   end
+  it"Debe existir una tirada para el humano" do
+      @ppt.respond_to?("jugadahumano").should == true
+  end
+  it"Debe existir una tirada para la maquina" do
+      @ppt.respond_to?("jugadamaquina").should == true
+  end
+end
